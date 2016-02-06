763330a7d6ef4c7c0d2f250eee6ebf1f2a825ab9
Scott Vitale <svvitale@gmail.com>
2016-02-05 17:15:59 -0700

    Add twilio response XML

--- havasu/views.py
+++ havasu/views.py
@@ -4,4 +4,9 @@ from django.views.generic import View
 
 class TwilioView(View):
     def get(self, request):
-        return HttpResponse("working")
+        response = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Hi! We are going to connect you to... Kate Vitale</Say>
+    <Dial>+13038425418</Dial>
+</Response>"""
+        return HttpResponse(response, content_type='text/xml')
