152bccbb23ac6acb7e7578b502e277b6d9000cde
Scott Vitale <svvitale@gmail.com>
2016-02-08 19:14:16 -0700

    Implement Twilio call status tracking

--- havasu/views.py
+++ havasu/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
 from django.views.generic import View
 
+from havasu.models import Call
+
 
 class TwilioView(View):
+
+    CALL_IN_PROGRESS_STATUS = [
+        'queued',
+        'ringing',
+        'in-progress'
+    ]
+
+    CALL_COMPLETED_STATUS = [
+        'busy',
+        'failed',
+        'no-answer',
+        'canceled'
+    ]
+
+    SUCCESS_STATUS = 'completed'
+
     def get(self, request):
         response = """<?xml version="1.0" encoding="UTF-8"?>
 <Response>
@@ -12,6 +30,10 @@ class TwilioView(View):
         return HttpResponse(response, content_type='text/xml')
 
     def post(self, request):
-        for key, val in request.POST.items():
-            print('%s = %s' % (key, val))
+
+        call = Call.objects.get(call_id=request.POST['CallSid'])
+        call.status = request.POST['CallStatus']
+        call.in_progress = call.status in TwilioView.CALL_IN_PROGRESS_STATUS
+        call.save()
+
         return HttpResponse()
